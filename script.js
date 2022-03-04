dd252c3cd251a802a6533715945d62fa03bfb9dc
Roman <romankp@gmail.com>
2022-03-04 16:42:47 -0500

    move functions out of jQuery init

--- script.js
+++ script.js
@@ -34,6 +34,25 @@ const updateForecast = daysArray => {
   });
 };
 
+const funnelForecastWeather = data => {
+  const { daily } = data;
+  const forecastDays = [];
+
+  for (let d = 1; d < 8; d++) {
+    const { dt } = daily[d];
+    const { weather, temp } = daily[d - 1];
+    forecastDays[d - 1] = {
+      day: weekArray[returnDayIndex(dt)],
+      desc: weather[0].description,
+      tempMax: Math.round(temp.max),
+      tempMin: Math.round(temp.min),
+      icon: weather[0].icon,
+    };
+  }
+
+  updateForecast(forecastDays);
+};
+
 const returnDayIndex = timeStamp => {
   // Returns numeric index value to provide associated day of the week
   const dateObj = new Date(timeStamp * 1000);
@@ -58,25 +77,6 @@ $(() => {
 
     updateBG(icon);
     updateCurrentWeather(desc, temp, icon);
-  };
-
-  const funnelForecastWeather = data => {
-    const { daily } = data;
-    const forecastDays = [];
-
-    for (let d = 1; d < 8; d++) {
-      const { dt } = daily[d];
-      const { weather, temp } = daily[d - 1];
-      forecastDays[d - 1] = {
-        day: weekArray[returnDayIndex(dt)],
-        desc: weather[0].description,
-        tempMax: Math.round(temp.max),
-        tempMin: Math.round(temp.min),
-        icon: weather[0].icon,
-      };
-    }
-
-    updateForecast(forecastDays);
   };
 
   // Update current weather section
