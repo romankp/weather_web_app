8a95d8402fddc73f06db3abe2f86b5f0973093ae
Roman <romankp@gmail.com>
2022-03-27 22:50:11 -0400

    move functions

--- main.js
+++ main.js
@@ -9,6 +9,16 @@ const dayDivs = document.getElementsByClassName('day');
 const weekArray = ['SUN', 'MON', 'TUES', 'WED', 'THUR', 'FRI', 'SAT'];
 
 // Saturate weather UI with data
+const funnelCurrentWeather = data => {
+  const {
+    current: { weather, temp },
+  } = data;
+  const { description, icon } = weather[0];
+  const roundedTemp = Math.round(temp);
+
+  updateBG(icon);
+  updateCurrentWeather(description, roundedTemp, icon);
+};
 
 // Update current weather section
 const updateCurrentWeather = (desc, temp, icon) => {
@@ -31,16 +41,23 @@ const updateCurrentWeather = (desc, temp, icon) => {
   }
 };
 
-const funnelCurrentWeather = data => {
-  const {
-    current: { weather, temp },
-  } = data;
-  // const { weather, temp } = current;
-  const { description, icon } = weather[0];
-  const roundedTemp = Math.round(temp);
+const funnelForecastWeather = data => {
+  const { daily } = data;
+  const forecastDays = [];
 
-  updateBG(icon);
-  updateCurrentWeather(description, roundedTemp, icon);
+  for (let d = 1; d < 8; d++) {
+    const { dt } = daily[d];
+    const { weather, temp } = daily[d - 1];
+    forecastDays[d - 1] = {
+      day: weekArray[returnDayIndex(dt)],
+      desc: weather[0].description,
+      tempMax: Math.round(temp.max),
+      tempMin: Math.round(temp.min),
+      icon: weather[0].icon,
+    };
+  }
+
+  updateForecast(forecastDays);
 };
 
 // Update the forecast section and each .day element
@@ -70,25 +87,6 @@ const updateForecast = daysArray => {
     descEl.innerText = desc;
     el.append(descEl);
   });
-};
-
-const funnelForecastWeather = data => {
-  const { daily } = data;
-  const forecastDays = [];
-
-  for (let d = 1; d < 8; d++) {
-    const { dt } = daily[d];
-    const { weather, temp } = daily[d - 1];
-    forecastDays[d - 1] = {
-      day: weekArray[returnDayIndex(dt)],
-      desc: weather[0].description,
-      tempMax: Math.round(temp.max),
-      tempMin: Math.round(temp.min),
-      icon: weather[0].icon,
-    };
-  }
-
-  updateForecast(forecastDays);
 };
 
 const returnDayIndex = timeStamp => {
